40052a15ad479a7526d49084a53f5d328ffc0da0
Alex Bertotto <ajb5206@gmail.com>
2021-10-09 11:33:51 -0700

    add controller files and code to Homecontroller

---
+++ HairSalon/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HairSalon.Controllers
+{
+	public class HomeController : Controller
+	{
+
+		[HttpGet("/")]
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
