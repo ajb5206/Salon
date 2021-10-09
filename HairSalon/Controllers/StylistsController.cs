55b7144786bf50ed94740f5af521704c6b1e3190
Alex Bertotto <ajb5206@gmail.com>
2021-10-09 12:42:10 -0700

    add details method to stylistscontroller

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -32,5 +32,11 @@ namespace HairSalon.Controllers
 			_db.SaveChanges();
 			return RedirectToAction("Index");
 		}
+
+		public ActionResult Details(int id)
+		{
+			Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+			return View(thisStylist);
+		}
 	}
 }
