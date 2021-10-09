4007bd0c873c2971c99226ea64bd47404c5270c0
Alex Bertotto <ajb5206@gmail.com>
2021-10-09 11:54:05 -0700

    Add code to stylists/create for 3 properties

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -3,6 +3,7 @@ namespace HairSalon.Models
 	public class Stylist
 	{
 		public int StylistId { get; set; }
+		public string StylistName { get; set; }
 		public string Description { get; set; }
 		public string Style {get; set; }
 	}
