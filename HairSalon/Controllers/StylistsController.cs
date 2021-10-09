db2ee7f4aac1fa85afabb720bb509482da88b702
Alex Bertotto <ajb5206@gmail.com>
2021-10-09 14:51:14 -0700

    add delete get and post actions to stylistscontroller

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -43,13 +43,28 @@ namespace HairSalon.Controllers
 		public ActionResult Edit(int id)
 		{
 			var thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
-			return View(thisStylist)
+			return View(thisStylist);
 		}
 
 		[HttpPost]
 		public ActionResult Edit(Stylist stylist)
 		{
 			_db.entry(stylist).State = EntityState.Modified;
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult Delete(int id)
+		{
+			Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+			return View(thisStylist);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+			_db.Stylists.Remove(thisStylist);
 			_db.SaveChanges();
 			return RedirectToAction("Index");
 		}
